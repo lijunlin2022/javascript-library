27642006877bff9c6aa51986caddd3ccfddca289
a-layfolk <1808150553@qq.com>
2021-08-12 23:09:47 +0800

    [feat] 完成 es5 LinkedList 的封装

--- src/es5/LinkedList.js
+++ src/es5/LinkedList.js
@@ -52,6 +52,98 @@ LinkedList.prototype.insert = function (position, data) {
   return true;
 }
 
+// get 方法, 获得第 position 个 Node 的 data
+LinkedList.prototype.get = function (position) {
+  // 越界判断, 和 insert 不同, get 的 postion 不能为 this.length
+  if (position < 0 || position > this.length - 1) {
+    return null;
+  }
+
+  var current = this.head;
+  for (var i = 0; i < position; i++) {
+    current = current.next;
+  }
+  return current.data;
+}
+
+// indexOf(element) 方法, 获取 data 的值为 element 的索引
+LinkedList.prototype.indexOf = function (element) {
+  var current = this.head;
+  for (var index = 0; index < this.length; index++) {
+    if (current.data === element) {
+      return index;
+    } else {
+      current = current.next;
+    }
+  }
+  return -1;
+}
+
+// update 方法, 更新 position 位置的 data
+LinkedList.prototype.update = function (position, element) {
+  if (position < 0 || position > this.length - 1) {
+    return false;
+  }
+  var current = this.head;
+  for (var i = 0; i < position; i++) {
+    current = current.next;
+  }
+  current.data = element;
+  return true;
+}
+
+LinkedList.prototype.removeAt = function (position) {
+  if (position < 0 || position > this.length - 1) {
+    return null;
+  }
+  var ret = null;
+  // 需要注意, 删除头节点和其他节点是不一样的
+  if (position === 0) {
+    ret = this.head.data;
+    this.head = this.head.next;
+  } else {
+    var previous = this.head;
+    for (var i = 0; i < position - 1; i++) {
+      previous = previous.next;
+    }
+    ret = previous.next.data;
+    previous.next = previous.next.next;
+  }
+  this.length -= 1;
+  return ret;
+}
+
+LinkedList.prototype.remove = function (element) {
+  if (this.head.data === element) {
+    this.head = this.head.next;
+    this.length -= 1;
+    return 0;
+  } else {
+    // 删除节点需要知道 previous 和 current 的位置,
+    // 直接从 index = 1 的位置开始
+    var previous = this.head;
+    var current = previous.next;
+    for (var index = 1; index < this.length; index++) {
+      if (current.data === element) {
+        previous.next = current.next;
+        this.length -= 1;
+        return index;
+      }
+      previous = previous.next;
+      current = current.next;
+    }
+    return -1;
+  }
+}
+
+LinkedList.prototype.isEmpty = function () {
+  return this.length === 0;
+}
+
+LinkedList.prototype.size = function () {
+  return this.length;
+}
+
 LinkedList.prototype.toString = function () {
   var arr = [];
   var current = this.head;
--- test/es5/LinkedList.test.js
+++ test/es5/LinkedList.test.js
@@ -17,3 +17,49 @@ test("测试 insert 方法", () => {
   list.insert(5, "Z");
   expect(list.toString()).toBe("[D, F, A, B, C, Z]");
 });
+
+test("测试 get 方法", () => {
+  expect(list.get(3)).toBe("B");
+  expect(list.get(5)).toBe("Z");
+  expect(list.get(0)).toBe("D");
+});
+
+test("测试 indexOf 方法", () => {
+  expect(list.indexOf("A")).toBe(2);
+  expect(list.indexOf("O")).toBe(-1);
+});
+
+test("测试 update 方法", () => {
+  expect(list.update(0, "P")).toBe(true);
+  expect(list.toString()).toBe("[P, F, A, B, C, Z]");
+  expect(list.update(-1, "X")).toBe(false);
+});
+
+test("测试 removeAt 方法", () => {
+  expect(list.removeAt(2)).toBe("A");
+  expect(list.toString()).toBe("[P, F, B, C, Z]");
+  expect(list.removeAt(-1)).toBe(null);
+  expect(list.removeAt(0)).toBe("P");
+  expect(list.toString()).toBe("[F, B, C, Z]");
+});
+
+test("测试 remove 方法", () => {
+  expect(list.remove("F")).toBe(0);
+  expect(list.toString()).toBe("[B, C, Z]");
+  expect(list.remove("C")).toBe(1);
+  expect(list.toString()).toBe("[B, Z]");
+});
+
+test("测试 isEmpty 方法", () => {
+  expect(list.isEmpty()).toBe(false);
+  list.removeAt(0);
+  list.removeAt(0);
+  expect(list.isEmpty()).toBe(true);
+});
+
+test("测试 size 方法", () => {
+  expect(list.size()).toBe(0);
+  list.append("1");
+  expect(list.size()).toBe(1);
+});
+
