3822c2d27b6026f363bfda2616879d88247810c8
a-layfolk <1808150553@qq.com>
2021-08-14 13:09:32 +0800

    [refacter] 重构 Stack 和 Queue 的测试用例

--- test/es5/Queue.test.js
+++ test/es5/Queue.test.js
@@ -1,35 +1,46 @@
 const { Queue } = require("../../src/es5/Queue");
 
-const queue = new Queue();
-
 test("测试 enqueue", () => {
+  const queue = new Queue();
   queue.enqueue(1);
   queue.enqueue(2);
   expect(queue.toString()).toBe("[1, 2]");
 });
 
 test("测试 dequeue", () => {
+  const queue = new Queue();
+  queue.enqueue(1);
+  queue.enqueue(2);
   expect(queue.dequeue()).toBe(1);
   expect(queue.toString()).toBe("[2]");
 });
 
-test("测试 front", () => {
+test("测试 front 和 back", () => {
+  const queue = new Queue();
   queue.enqueue(3);
   queue.enqueue(4);
-  expect(queue.front()).toBe(2);
+  expect(queue.front()).toBe(3);
   expect(queue.back()).toBe(4);
 });
 
 test("测试 size", () => {
-  expect(queue.size()).toBe(3);
+  const queue = new Queue();
+  expect(queue.size()).toBe(0);
+  queue.enqueue(1);
+  expect(queue.size()).toBe(1);
 });
 
 test("测试 isEmpty", () => {
-  expect(queue.isEmpty()).toBe(false);
-  queue.dequeue();
-  queue.dequeue();
-  queue.dequeue();
+  const queue = new Queue();
   expect(queue.isEmpty()).toBe(true);
+  queue.enqueue(1);
+  expect(queue.isEmpty()).toBe(false);
 });
 
-// toString() 在之前的例子中已经测验过了
+test("测试 toString", () => {
+  const queue = new Queue();
+  expect(queue.toString()).toBe("[]");
+  queue.enqueue("A");
+  queue.enqueue("B");
+  expect(queue.toString()).toBe("[A, B]");
+});
--- test/es5/Stack.test.js
+++ test/es5/Stack.test.js
@@ -1,32 +1,39 @@
 const { Stack } = require("../../src/es5/Stack");
 
-const stack = new Stack();
-
 test("测试 push", () => {
+  const stack = new Stack();
   stack.push(6);
   stack.push(5);
   expect(stack.toString()).toBe("[6, 5]");
-});
+}); 
 
 test("测试 pop", () => {
+  const stack = new Stack();
+  stack.push(6);
+  stack.push(5);
   const top = stack.pop();
   expect(top).toBe(5);
   expect(stack.toString()).toBe("[6]");
 });
 
 test("测试 peek", () => {
+  const stack = new Stack();
+  stack.push(6);
+  stack.push(5);
   const top = stack.peek();
-  expect(top).toBe(6);
-  expect(stack.toString()).toBe("[6]");
+  expect(top).toBe(5);
+  expect(stack.toString()).toBe("[6, 5]");
 });
 
 test("测试 isEmpty", () => {
-  expect(stack.isEmpty()).toBe(false);
-  stack.pop();
+  const stack = new Stack();
   expect(stack.isEmpty()).toBe(true);
+  stack.push(1);
+  expect(stack.isEmpty()).toBe(false);
 });
 
 test("测试 size", () => {
+  const stack = new Stack();
   stack.push(7);
   stack.push(8);
   stack.push(9);
@@ -34,4 +41,10 @@ test("测试 size", () => {
   expect(stack.toString()).toBe("[7, 8, 9]");
 });
 
-// toString() 之前已经测试过了
+test("测试 toString", () => {
+  const stack = new Stack();
+  expect(stack.toString()).toBe("[]");
+  stack.push(1);
+  stack.push(2);
+  expect(stack.toString()).toBe("[1, 2]");
+});
