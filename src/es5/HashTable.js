b25ddd66b2e700be1154ec19e7d40987aec042dd
a-layfolk <1808150553@qq.com>
2021-08-17 13:24:17 +0800

    [feat] 添加 HashTable 的 set 和 toString 方法

--- src/es5/HashTable.js
+++ src/es5/HashTable.js
@@ -1,10 +1,16 @@
+function HashTable() {
+  this.storage = [];  // 哈希表中的大数组
+  this.count = 0; // 哈希表中已经保存的元素, 方便计算 loadFactor
+  this.limit = 7; // 哈希表的容量
+}
+
 /**
  * 哈希函数, 生成 index
  * @param {String} str 
  * @param {Number} size 
  * @returns {Number}
  */
-function hashFun(str, size) {
+HashTable.prototype.hashFun = function (str, size) {
   var hashCode = 0;
   var primeNum = 37; // 37 是一个用于求 hashCode 的质数
   // 霍纳算法, 求 hashCode 的值
@@ -15,12 +21,56 @@ function hashFun(str, size) {
   return index;
 }
 
-function HashTable {
-  this.storage = [];  // 哈希表中的大数组
-  this.count = 0; // 哈希表中已经保存的元素, 方便计算 loadFactor
-  this.limit = 7; // 哈希表的容量
+HashTable.prototype.set = function (key, value) {
+  // 1. 根据 key 获取 index
+  var index = this.hashFun(key, this.limit);
+  // 2. 根据 index 获取 bucket
+  var bucket = this.storage[index];
+  // 如果 bucket 不存在, 则创建 bucket
+  if (bucket == null) {
+    bucket = [];
+    this.storage[index] = bucket;
+  }
+  // 3. 判断是否修改数据
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+    if (tuple[0] === key) {
+      tuple[1] = value;
+      return;
+    }
+  }
+  // 4. 如果不是修改数据, 则新增数据
+  bucket.push([key, value]);
+}
+
+HashTable.prototype.toString = function () {
+  var sArr = [];
+  for (var i = 0; i < this.limit; i++) {
+    var bucket = this.storage[i];
+    if (bucket == null) {
+      sArr.push("[]");
+    } else {
+      var bArr = [];
+      for (var j = 0; j < bucket.length; j++) {
+        var tuple = bucket[j];
+        if (tuple == null) {
+          bArr.push("[]");
+        } else {
+          bArr.push(
+            "[" +
+            JSON.stringify(tuple[0]) +
+            ", " +
+            JSON.stringify(tuple[1]) +
+            "]"
+          );
+        }
+      }
+      sArr.push("[" + bArr.join(", ") + "]");
+    }
+  }
+  return "[" + sArr.join(", ") + "]";
 }
 
 module.exports = {
-  hashFun
+  HashTable
 };
--- test/es5/HashTable.test.js
+++ test/es5/HashTable.test.js
@@ -1,8 +1,23 @@
-const { hashFun } = require("../../src/es5/HashTable");
+const { HashTable } = require("../../src/es5/HashTable");
 
-test("测试 hashFunc", () => {
-  expect(hashFun("abc", 7)).toBe(4);
-  expect(hashFun("cba", 7)).toBe(3);
-  expect(hashFun("nba", 7)).toBe(5);
-  expect(hashFun("mba", 7)).toBe(1);
+test("测试 hashFun", () => {
+  const ht = new HashTable();
+  expect(ht.hashFun("abc", 7)).toBe(4);
+  expect(ht.hashFun("cba", 7)).toBe(3);
+  expect(ht.hashFun("nba", 7)).toBe(5);
+  expect(ht.hashFun("mba", 7)).toBe(1);
+});
+
+test("测试 toString", () => {
+  const ht = new HashTable();
+  console.log(ht.toString());
+  ht.set("A", 1);
+  console.log(ht.toString());
+  ht.set("B", 2);
+  console.log(ht.toString());
+  ht.set("C", {
+    name: "andy",
+    age: 20
+  });
+  console.log(ht.toString());
 });
