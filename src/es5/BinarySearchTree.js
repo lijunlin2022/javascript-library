a6d704eea90172c7057a59a7d531247358b6df88
a-layfolk <1808150553@qq.com>
2021-08-18 21:31:54 +0800

    [feat] 添加 remove 方法

--- src/es5/BinarySearchTree.js
+++ src/es5/BinarySearchTree.js
@@ -110,6 +110,142 @@ BinarySearchTree.prototype.postorderTraversalToString = function () {
   return "[" + arr.join(", ") + "]";
 }
 
+BinarySearchTree.prototype.max = function () {
+  var node = this.root;
+  
+  if (node == null) {
+    return null;
+  }
+
+  while (node.right != null) {
+    node = node.right;
+  }
+
+  return node.key;
+}
+
+BinarySearchTree.prototype.min = function () {
+  var node = this.root;
+
+  if (node == null) {
+    return null;
+  }
+
+  while (node.left != null) {
+    node = node.left;
+  }
+
+  return node.key;
+}
+
+BinarySearchTree.prototype.search = function (key) {
+  var node = this.root;
+  while (node != null) {
+    if (key < node.key) {
+      node = node.left;
+    } else if (key > node.key) {
+      node = node.right;
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
+
+BinarySearchTree.prototype.remove = function (key) {
+  if (this.root == null) {
+    return false;
+  }
+
+  var current = this.root;
+  var parent = null;
+  var isLeftChild = true;
+
+  // 查找节点
+  while (key != current.key) {
+    parent = current;
+    if (key < current.key) {
+      isLeftChild = true;
+      current = current.left;
+    } else {
+      isLeftChild = false;
+      current = current.right;
+    }
+
+    // 如果没找到节点
+    if (current == null) {
+      return false;
+    }
+  }
+
+  // 找到了节点
+  // 如果节点是叶子节点
+  if (current.left == null && current.right == null) {
+    // 如果节点是叶子节点也是根节点
+    if (current === this.root) {
+      this.root = null;
+    } else if (isLeftChild) {
+      parent.left = null;
+    } else {
+      parent.right = null;
+    }
+  }
+  else if (current.right == null) { // 如果节点只有一个左子节点
+    if (current === this.root) {
+      this.root = current.left;
+    } else if (isLeftChild) {
+      parent.left = current.left;
+    } else {
+      parent.right = current.left;
+    }
+  }
+  else if (current.left == null) { // 如果节点只有一个右子节点
+    if (current === this.root) {
+      this.root = current.right;
+    } else if (isLeftChild) {
+      parent.left = current.right;
+    } else {
+      parent.right = current.right;
+    }
+  }
+  else { // 如果节点有两个子节点
+    var successor = this.getSuccessor(current);
+
+    if (current === this.root) {
+      this.root = successor;
+    } else if (isLeftChild) {
+      parent.left = successor;
+    } else {
+      parent.right = successor;
+    }
+
+    successor.left = current.left;
+  }
+
+  return true;
+}
+
+BinarySearchTree.prototype.getSuccessor = function (delNode) {
+  var successorParent = delNode;
+  var successor = delNode;
+  var current = delNode.right;
+
+  // 寻找 delNode 右子树的最小子节点
+  while (current != null) {
+    successorParent = successor;
+    successor = current;
+    current = current.left;
+  }
+
+  // 如果找到的后继不是 delNode 直接的有子节点
+  if (successor !== delNode.right) {
+    successorParent.left = successor.right;
+    successor.right = delNode.right;
+  }
+
+  return successor;
+}
+
 module.exports = {
   BinarySearchTree
 };
--- test/es5/BinarySearchTree.test.js
+++ test/es5/BinarySearchTree.test.js
@@ -64,3 +64,90 @@ test("测试 postorderTraversalToString 方法", () => {
   bst.insert(6);
   expect(bst.postorderTraversalToString()).toBe("[3, 6, 5, 8, 10, 9, 7, 12, 14, 13, 18, 25, 20, 15, 11]");
 });
+
+test("测试 max 方法", () => {
+  const bst = new BinarySearchTree();
+  expect(bst.max()).toBe(null);
+  bst.insert(7);
+  expect(bst.max()).toBe(7);
+  bst.insert(5);
+  expect(bst.max()).toBe(7);
+  bst.insert(8);
+  expect(bst.max()).toBe(8);
+});
+
+test("测试 min 方法", () => {
+  const bst = new BinarySearchTree();
+  expect(bst.max()).toBe(null);
+  bst.insert(7);
+  expect(bst.min()).toBe(7);
+  bst.insert(5);
+  expect(bst.min()).toBe(5);
+  bst.insert(8);
+  expect(bst.min()).toBe(5);
+});
+
+test("测试 search 方法", () => {
+  const bst = new BinarySearchTree();
+  expect(bst.search(0)).toBe(false);
+  bst.insert(3);
+  expect(bst.search(3)).toBe(true);
+  bst.insert(1);
+  bst.insert(8);
+  expect(bst.search(9)).toBe(false);
+});
+
+test("测试 remove 方法", () => {
+  const bst = new BinarySearchTree();
+  expect(bst.remove(0)).toBe(false); // 测试删除空节点
+  bst.insert(0);
+  expect(bst.remove(0)).toBe(true); // 测试删除叶子节点同时也是根节点
+  bst.insert(0);
+  bst.insert(1);
+  expect(bst.remove(1)).toBe(true); // 测试删除叶子节点
+  bst.remove(0);
+
+  bst.insert(3);
+  bst.insert(2);
+  bst.insert(1);
+  expect(bst.remove(2)).toBe(true); // 测试删除有一个左子节点的节点
+  expect(bst.remove(3)).toBe(true);
+  bst.remove(1);
+
+  bst.insert(4);
+  bst.insert(5);
+  bst.insert(6);
+  expect(bst.remove(5)).toBe(true); // 测试删除有一个右子节点的节点
+  expect(bst.remove(4)).toBe(true);
+  bst.remove(6);
+
+  // 测试删除有两个子节点的情况
+  bst.insert(2); // 根节点
+  bst.insert(1);
+  bst.insert(3);
+  expect(bst.inorderTraversalToString()).toBe("[1, 2, 3]");
+  expect(bst.remove(2)).toBe(true);
+  expect(bst.inorderTraversalToString()).toBe("[1, 3]");
+  bst.remove(1);
+  bst.remove(3);
+
+  // 测试找到的后缀不是 delNode 直接的子节点
+  bst.insert(11);
+  bst.insert(7);
+  bst.insert(15);
+  bst.insert(5);
+  bst.insert(3);
+  bst.insert(9);
+  bst.insert(8);
+  bst.insert(10);
+  bst.insert(13);
+  bst.insert(12);
+  bst.insert(14);
+  bst.insert(20);
+  bst.insert(18);
+  bst.insert(25);
+  bst.insert(6);
+  expect(bst.inorderTraversalToString()).toBe("[3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 18, 20, 25]");  
+  bst.remove(15)
+  expect(bst.inorderTraversalToString()).toBe("[3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 18, 20, 25]");
+});
