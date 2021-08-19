a6a6103d6a16a72eaf5941c55ab1b4ff3b371815
a-layfolk <1808150553@qq.com>
2021-08-19 12:15:47 +0800

    [feat] 添加 bfs 搜索算法

--- src/es5/Graph.js
+++ src/es5/Graph.js
@@ -24,6 +24,52 @@ Graph.prototype.toString = function () {
   return resArr.join("\n");
 }
 
+Graph.prototype.initializeColor = function () {
+  var colorMap = new Map();
+  for (var i = 0; i < this.vertexes.length; i++) {
+    colorMap.set(this.vertexes[i], "white");
+  }
+  return colorMap;
+}
+
+Graph.prototype.bfs = function (initV, handler) {
+  // 初始化颜色
+  var colorMap = this.initializeColor();
+  var queue = [];
+  // 将顶点加入队列
+  queue.push(initV);
+
+  while (queue.length !== 0) {
+    // 从队列中取出一个顶点
+    var v = queue.shift();
+    // 获取该节点相邻的其他节点
+    var vAdjList = this.edges.get(v);
+    // 将顶点的颜色变为灰色
+    colorMap.set(v, "gray");
+
+    // 将没有访问过的相邻节点加入队列
+    vAdjList.forEach(item => {
+      if (colorMap.get(item) === "white") {
+        colorMap.set(item, "gray");
+        queue.push(item);
+      }
+    });
+
+    // 处理节点
+    handler(v);
+
+    colorMap.set(v, "black");
+  }
+}
+
+Graph.prototype.bfsToString = function (initV) {
+  var arr = [];
+  this.bfs(initV, function (v) {
+    arr.push(v);
+  });
+  return "[" + arr.join(", ") + "]";
+}
+
 module.exports = {
   Graph
 };
--- test/es5/Graph.test.js
+++ test/es5/Graph.test.js
@@ -38,3 +38,22 @@ test("测试 graph 的 addEdge 方法", () => {
      H->D
      I->E`.split(" ").join(""));
 });
+
+test("测试 bfsToString 方法", () => {
+  const myVertex = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+  const graph = new Graph();
+  myVertex.forEach(item => {
+    graph.addVertex(item);
+  });
+  graph.addEdge("A", "B");
+  graph.addEdge("A", "C");
+  graph.addEdge("A", "D");
+  graph.addEdge("C", "D");
+  graph.addEdge("C", "G");
+  graph.addEdge("D", "G");
+  graph.addEdge("D", "H");
+  graph.addEdge("B", "E");
+  graph.addEdge("B", "F");
+  graph.addEdge("E", "I");
+  expect(graph.bfsToString("A")).toBe("[A, B, C, D, E, F, G, H, I]");
+});
